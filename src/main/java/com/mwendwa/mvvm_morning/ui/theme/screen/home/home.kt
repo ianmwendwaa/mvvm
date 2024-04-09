4a247f128626b3887c379636618367bc2a820f85
ianmwendwaa <williamsian845@gmail.com>
2024-04-09 10:58:35 +0300

    initial commit

--- src/main/java/com/mwendwa/mvvm_morning/ui/theme/screen/home/home.kt
+++ src/main/java/com/mwendwa/mvvm_morning/ui/theme/screen/home/home.kt
@@ -3,7 +3,11 @@ package com.mwendwa.mvvm_morning.ui.theme.screen.Home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,7 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.nio.file.WatchEvent
 
 @Composable
 fun HomeScreen() {
@@ -27,6 +33,11 @@ fun HomeScreen() {
             fontFamily = FontFamily.Serif,
             fontSize = 30.sp
         )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedButton(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()) {
+            Text(text = "About")
+        }
 
     }
 
